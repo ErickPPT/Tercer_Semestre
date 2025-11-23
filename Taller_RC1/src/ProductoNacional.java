b3bcc72bad7533d313541966077c78eea97e6324
ErickPPT <paul.pisuna2006@gmail.com>
2025-11-22 21:39:44 -0500

    Refactor ProductoNacional constructor parameters

--- Taller_RC1/src/ProductoNacional.java
+++ Taller_RC1/src/ProductoNacional.java
@@ -2,11 +2,12 @@ public class ProductoNacional extends Producto {
     /** Creacion del atributo impuesto nacional*/
     private double impuestoNacional=0.12;
 
-    public ProductoNacional(int ID, String nombre, double precio, double precioCompra, int cantidadDisponible, int cantidadMinima, int diaReabastecimiento, int tiempoEntrega, int espacioMaximo, int tipo) {
-        super(ID, nombre, precio, precioCompra, cantidadDisponible, cantidadMinima, diaReabastecimiento, tiempoEntrega, espacioMaximo, tipo);
-
+    /**Constructor*/
+    public ProductoNacional(int ID, String nombre, double precioCompra, double precio, int espacioMaximo, int cantidadDisponible, int cantidadMinima, int diaReabastecimiento, int tiempoEntrega, int tipo) {
+        super(ID, nombre, precioCompra, precio, espacioMaximo, cantidadDisponible, cantidadMinima, diaReabastecimiento, tiempoEntrega, tipo);
     }
 
+    /** Metodo propio del autor*/
     public double calcularCostoreabastecimiento(int cantidad){
         double c=cantidad*getPrecioCompra();
         return c+c*impuestoNacional;
